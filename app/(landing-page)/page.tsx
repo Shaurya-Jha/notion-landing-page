16fafb6a298ae004d965e5d44ad49fb22deb09a8
Shaurya Jha <shauryajha1602@gmail.com>
2024-01-25 23:03:38 +0530

    added shadcn and planned layout

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,29 @@
+
+
+const LandingPage = () => {
+  return (
+    <div className="">
+        navbar
+
+        herosection
+
+        second section
+
+        third section
+
+        fourth section
+
+        fifth section
+
+        sixth section
+
+        seventh section
+
+        get started section
+
+        footer section
+    </div>
+  )
+}
+
+export default LandingPage
